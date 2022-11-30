3205adf53430ffb956909357ea87cd39a80e4a19
yanyiik99 <aripramana574@gmail.com>
2022-12-01 01:00:52 +0800

    konfigurasi redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import * as Page from './Page';
 import * as Component from './components';
-import { createStore } from "redux";
-import reducer from "./Bootstraps/reducer";
+import * as Layout from './Layouts'
+// REDUX
+import createSagaMiddleWare from 'redux-saga';
+import rootReducer from './Bootstraps/rootReducer';
 import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import rootSaga from './Bootstraps/sagas';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 function App() {
 
-  const store = createStore(reducer)
+  const sagaMiddleWare = createSagaMiddleWare();
+  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleWare)));
+  sagaMiddleWare.run(rootSaga)
 
 
   const pages = [
@@ -29,16 +36,17 @@ function App() {
       element: <Page.Project />
     },
   ]
-  
+
   return (
     <Provider store={store}>
       <Router>
         <Component.ScrollToTop>
+          <Layout.Navbar /> 
           <Routes>
             {
               pages?.map((el, i)=>{
                 return(
-                  <Route key={i} exact path={el.path} element={el.element} />
+                  <Route key={i} path={el.path} element={el.element} />
                 )})
             }
           </Routes>
--- src/Page/Achievement/index.jsx
+++ src/Page/Achievement/index.jsx
@@ -1,13 +1,34 @@
 import * as Layout from '../../Layouts';
-import * as Page from '../index'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCards } from "swiper";
+import "swiper/css/effect-cards";
+import 'swiper/css';
+import './index.css';
 
 function Achievement() {
   return (
     <div style={{ height: "1000px" }}>
       <Layout.Navbar />
-      <Page.Maintenance />
+      <div className='bg-red-500 w-full h-24 pt-56'>
+      <Swiper
+          effect={"cards"}
+          grabCursor={true}
+          modules={[EffectCards]}
+          className="mySwiper"
+      >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      </div>
     </div>
   )
 }
 
-export default Achievement
+export default Achievement;
